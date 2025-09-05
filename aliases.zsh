9f596a323a06cc7ff516a85b1978f18fb51cdaed
Jakub Górski <jakubg1527@proton.me>
2025-09-05 19:35:20 +0200

    Add tmux aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,17 @@
 alias src='source ./.venv/bin/activate'
 alias e="$EDITOR"
+alias tcode='tmux new-session \; \
+  split-window -h -p 35 \; \
+  select-pane -t 1 \; \
+  split-window -v -p 40 \; \
+  select-pane -t 0 \; \
+  send-keys -t 0 "nvim ./" C-m'
+alias tpy='tmux new-session \; \
+  split-window -h -p 35 \; \
+  send-keys -t 1 "source ./.venv/bin/activate" C-m \; \
+  select-pane -t 1 \; \
+  split-window -v -p 40 \; \
+  send-keys -t 2 "source ./.venv/bin/activate" C-m \; \
+  select-pane -t 0 \; \
+  send-keys -t 0 "source ./.venv/bin/activate" C-m \; \
+  send-keys -t 0 "nvim ./" C-m'
