9e2d68a94b1889c565ce4c165465dec9fc28c3a9
Jakub Górski <jakubg1527@gmail.com>
2025-09-05 19:27:28 +0200

    Move nvim load to zshrc

--- exports.zsh
+++ exports.zsh
@@ -5,8 +5,3 @@ elif [[ -z "$DISPLAY" && -z "$WAYLAND_DISPLAY" ]]; then
 else
   export EDITOR='nvim'
 fi
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
