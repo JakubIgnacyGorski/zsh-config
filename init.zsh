97aa823d429e4ac1e33fe92a6c29342b3f0a00aa
Jakub Górski <jakubg1527@gmail.com>
2026-01-18 17:58:30 +0100

    Make init file static

--- init.zsh
+++ init.zsh
@@ -1,9 +1,17 @@
-CONFIG_DIR="${0:A:h}"
-CURRENT_FILE="${(%):-%N}"
-CURRENT_FILE="$(basename $CURRENT_FILE)"
+# CONFIG_DIR="${0:A:h}"
+# CURRENT_FILE="${(%):-%N}"
+# CURRENT_FILE="$(basename $CURRENT_FILE)"
+#
+# for file in $CONFIG_DIR/*.zsh ; do
+#     if [[ "$(basename $file)" != "$CURRENT_FILE" ]] ; then
+#         source $file
+#     fi
+# done
 
-for file in $CONFIG_DIR/*.zsh ; do
-    if [[ "$(basename $file)" != "$CURRENT_FILE" ]] ; then
-        source $file
-    fi
+CONFIG_DIR="$HOME/.config/zsh"
+
+FILES_TO_SOURCE=("aliases.zsh" "exports.zsh" "functions.zsh")
+
+for file in $FILES_TO_SOURCE; do
+  source "$CONFIG_DIR/$file"
 done
