846a81906a4b86bab1b363d1c0eac3ca62a40fb6
Jakub Górski <jakubg1527@proton.me>
2026-01-26 16:45:19 +0100

    Remove internet access from bentopdf

--- aliases.zsh
+++ aliases.zsh
@@ -15,4 +15,12 @@ alias tpy='tmux new-session \; \
   send-keys -t 0 "source ./.venv/bin/activate" C-m \; \
   send-keys -t 0 "nvim ./" C-m'
 alias lg="lazygit"
-alias bentopdf='echo " > Open http://localhost:3000 to access BentoPDF" && (sleep 1 && xdg-open http://localhost:3000 >/dev/null 2>&1) & podman run --rm -p 127.0.0.1:3000:8080 --security-opt=no-new-privileges ghcr.io/alam00000/bentopdf:latest'
+alias bentopdf='echo " > Open http://localhost:3000 to access BentoPDF"
+  podman network create --internal no-internet-bentopdf
+  (sleep 1 && xdg-open http://localhost:3000 >/dev/null 2>&1) &
+  podman run --rm \
+    --network no-internet-bentopdf \
+    -p 127.0.0.1:3000:8080 \
+    --security-opt=no-new-privileges \
+    ghcr.io/alam00000/bentopdf:latest
+  podman network remove no-internet-bentopdf'
