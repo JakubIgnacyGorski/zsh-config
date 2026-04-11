16d3ea0c7c7652abc1e2692a61931908cbd149b8
Jakub Górski <jakubg1527@gmail.com>
2026-04-11 19:43:52 +0200

    Add auto tmux session creation

--- functions.zsh
+++ functions.zsh
@@ -5,3 +5,21 @@ function y() {
 	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
 	rm -f -- "$tmp"
 }
+
+# Auto create new tmux session when can't attach to existing one
+function tmuxa() {
+    sessions=$(tmux list-sessions -F "#{session_name}" 2>/dev/null)
+
+    if [ -z "$sessions" ]; then
+        tmux new-session
+        return
+    fi
+
+    target=$(echo "$sessions" | grep -E '^[0-9]+$' | sort -n | head -n 1)
+
+    if [ -z "$target" ]; then
+        tmux attach-session -t "$(echo "$sessions" | head -n 1)"
+    else
+        tmux attach-session -t "$target"
+    fi
+}
