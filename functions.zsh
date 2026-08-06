aed77530dd915620c008aa68f87897ab1a8c6f09
Jakub Górski <jakubg1527@proton.me>
2026-08-06 16:43:36 +0200

    Clear terminal after venv activation

--- functions.zsh
+++ functions.zsh
@@ -43,10 +43,10 @@ function tcode() {
 function tpy() {
   tmux new-session \; \
     split-window -h -p 35 \; \
-    send-keys -t 1 "source ./.venv/bin/activate" C-m \; \
+    send-keys -t 1 "source ./.venv/bin/activate && clear" C-m \; \
     select-pane -t 1 \; \
     split-window -v -p 40 \; \
-    send-keys -t 2 "source ./.venv/bin/activate" C-m \; \
+    send-keys -t 2 "source ./.venv/bin/activate && clear" C-m \; \
     select-pane -t 0 \; \
     send-keys -t 0 "source ./.venv/bin/activate" C-m \; \
     send-keys -t 0 "nvim ./" C-m
